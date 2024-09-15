0cce657ca959c148a1c47afe8b18a59479a980f9
ministrov <61511030+ministrov@users.noreply.github.com>
2024-09-15 18:35:38 +0300

    feat: grab the value of the input and save in to state

--- src/components/enterForm/EnterFrom.jsx
+++ src/components/enterForm/EnterFrom.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import EnterInput from '../enterInput/EnterInput';
 import Heading from '../heading/Heading';
 import Button from '../button/Button';
 import styles from './EnterForm.module.css';
 
 export default function EnterFrom() {
+  const [ value, setValue ] = useState('');
+  const inputRef = useRef(null);
+
   const onClickHandler = () => {
     console.log(`${this}: button is clicked`);
   };
+
+  const onChangeHandler = (event) => {
+    setValue(event.target.value);
+  };
+
+  // useEffect(() => {
+  //   console.log(value);
+  // }, [value]);
+
   return (
     <div className={styles['enter-form-wrapper']}>
       <Heading text={'Вход'}/>
       <form className={styles['enter-form']} action="#">
-        <EnterInput/>
+        <EnterInput ref={inputRef.current} onChange={onChangeHandler} values={value}/>
         <Button content={'Войти в профиль'} className={'button-big'} onClick={onClickHandler}/>
       </form>
     </div>
--- src/components/enterInput/EnterInput.jsx
+++ src/components/enterInput/EnterInput.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import styles from './EnterInput.module.css';
 
-export default function EnterInput() {
+const EnterInput = forwardRef(function EnterInput({ values, onChange }, ref) {
   return (
     <label className={styles['enter-input']} htmlFor="name">
-      <input type="text" name='name' placeholder='Ваше имя' />
+      <input type="text" name='name' placeholder='Ваше имя' onChange={onChange} value={values} ref={ref} />
     </label>
   )
-}
+});
+
+export default EnterInput;
