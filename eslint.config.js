0f80eead02b6c99c5d05448f46b7fd8a543779f7
unknown <ministrov2018@gmail.com>
2024-09-26 10:37:24 +0300

    fix: fixing error in eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
+// import js from '@eslint/js'
 import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+// import react from 'eslint-plugin-react'
+// import reactHooks from 'eslint-plugin-react-hooks'
+// import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
   { ignores: ['dist'] },
@@ -18,21 +18,21 @@ export default [
       },
     },
     settings: { react: { version: '18.3' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
+    // plugins: {
+    //   react,
+    //   'react-hooks': reactHooks,
+    //   'react-refresh': reactRefresh,
+    // },
+    // rules: {
+    //   ...js.configs.recommended.rules,
+    //   ...react.configs.recommended.rules,
+    //   ...react.configs['jsx-runtime'].rules,
+    //   ...reactHooks.configs.recommended.rules,
+    //   'react/jsx-no-target-blank': 'off',
+    //   'react-refresh/only-export-components': [
+    //     'warn',
+    //     { allowConstantExport: true },
+    //   ],
+    // },
   },
 ]
