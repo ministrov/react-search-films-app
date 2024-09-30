e6df4c3178c3f23f106971dde9acb34e21b8313b
unknown <ministrov2018@gmail.com>
2024-09-30 10:49:09 +0300

    feat: create favoritesPage component and mount it

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/mainPage/MainPage";
 import LoginPage from "./pages/loginPage/LoginPage";
+import FavoritesPage from "./pages/favoritesPage/FavoritesPage";
 import { IFilms } from "./types";
 
 type AppProps = {
@@ -8,12 +9,11 @@ type AppProps = {
 }
 
 function App({ films }: AppProps) {
-  
-
   return (
     <Routes>
       <Route path="/" element={<MainPage films={films}/>}/>
       <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/favorites" element={<FavoritesPage />}/>
     </Routes>  
   );
 }
---
+++ src/pages/favoritesPage/FavoritesPage.tsx
@@ -0,0 +1,10 @@
+import Heading from "../../components/heading/Heading"
+
+export default function FavoritesPage() {
+  return (
+    <div>
+      <Heading text={'Избранное'}/>
+      FavoritesPage
+    </div>
+  )
+}
