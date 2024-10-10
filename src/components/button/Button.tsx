2b4699cde930f6a1fa12ce83036f24c2ce5d683d
Zhila <ministrov2018@gmail.com>
2024-10-10 15:48:32 +0300

    feat: keep on operating on search realisation

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,18 +1,19 @@
+import { ReactNode } from 'react';
 import cn from 'classnames';
 import styles from './Button.module.css';
 
 type ButtonProps = {
-  content: string;
-  onClick: () => void
+  children: ReactNode
+  // onClick: (e: MouseEvent) => void
   className: string | null
 }
 
-export default function Button({ content, onClick, className }: ButtonProps) {
+export default function Button({ children, className, ...props }: ButtonProps) {
   return (
     <button className={cn(styles['button'], {
       [styles['button-big']]: className
-    })} onClick={onClick}>
-      {content}
+    })} {...props}>
+      {children}
     </button>
   )
 }
--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import Button from "../../components/button/Button";
 import Heading from "../../components/heading/Heading";
 import Paragraph from "../../components/paragraph/Paragraph";
@@ -13,31 +13,36 @@ const PREFIX = 'https://search.imdbot.workers.dev';
 export default function MainPage() {
   const [isLogged, setIsLogged] = useState(true);
   const [films, setFilms] = useState<FilmsDescription[]>([]);
+  const [search, setSearch] = useState<string>('');
   // const params = useParams();
   // console.log(params);
 
-  const getFilms = async () => {
-    try {
-      const res = await fetch(`${PREFIX}?q=${'#'}`);
-      if (!res.ok) {
+  useEffect(() => {
+    const getFilms = async (name?: string) => {
+      try {
+        const res = await fetch(`${PREFIX}?q=${search}`);
+        if (!res.ok) {
+          return;
+        }
+        const data = await res.json() as FilmsDescription[];
+        setFilms(data);
+
+      } catch (e) {
+        console.error(e);
         return;
       }
-      const data = await res.json() as FilmsDescription[];
-      setFilms(data);
-
-    } catch (e) {
-      console.error(e);
-      return;
-    }
-  };
-
-  useEffect(() => {
+    };
     // getFilms();
     console.log(films);
   }, []);
 
+  const updateFilter = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+    console.log(event.target.value);
+  };
+
   const onClickHandler = () => {
-    setIsLogged(false);
+    console.log('click');
   };
 
   return (
@@ -49,11 +54,12 @@ export default function MainPage() {
           Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.
         </Paragraph>
         <div className='left-box-bottom'>
-          <SearchInput />
+          <SearchInput onChange={updateFilter} value={search} />
           <Button
-            content={'Искать'}
-            onClick={onClickHandler}
-            className={null} />
+            // onClick={onClickHandler}
+            className={null}>
+            Искать
+          </Button>
         </div>
       </div>
       <div className='films-wrapper'>
