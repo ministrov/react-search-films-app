3f35a112127f4356d9453ccb72cdfbdbc87ff43a
Zhila <ministrov2018@gmail.com>
2024-10-26 12:17:31 +0300

    fix: fixing enter icon atribute and login page component to remove unnessecary state

--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -1,22 +1,18 @@
 import { useState, useEffect } from "react";
 import EnterFrom from "../../components/enterForm/EnterFrom";
-// import InfoPanel from "../../components/infoPanel/InfoPanel";
 import useLocaleStorage from "../../hooks/use-local-storage.hook";
 
 export default function LoginPage() {
   const [profiles, setProfiles] = useLocaleStorage('user-profile');
   const [inputValue, setInputValue] = useState('');
-  const [profile, setProfile] = useState({});
-
-  console.log(profile);
 
   const onSubmitHandler = () => {
-    if (!profiles && profiles !== 'undefined') {
-      setProfiles([{ name: inputValue.trim(), isLogged: true }]);
-    } else {
-      setProfiles([...profiles as [{ name: string, isLogged: boolean }], { name: inputValue.trim(), isLogged: true }]);
-      setProfile(...profiles as [{ name: string, isLogged: boolean }]);
-    }
+    // if (!profiles && profiles !== 'undefined') {
+    //   setProfiles([{ name: inputValue.trim(), isLogged: true }]);
+    // } else {
+    //   setProfiles([...profiles as [{ name: string, isLogged: boolean }], { name: inputValue.trim(), isLogged: true }]);
+    //   setProfile(...profiles as [{ name: string, isLogged: boolean }]);
+    // }
   };
 
   useEffect(() => {
