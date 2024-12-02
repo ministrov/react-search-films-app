be5a2776106790441b2d874a625544c8ccab0732
Zhila <ministrov2018@gmail.com>
2024-12-02 16:56:42 +0300

    feature: create and add to project embla carousel library

--- src/pages/aboutPage/AboutPage.tsx
+++ src/pages/aboutPage/AboutPage.tsx
@@ -1,4 +1,5 @@
 import Heading from '../../components/heading/Heading';
+import FeedbackFrom from "../../components/feedbackForm/FeedbackFrom";
 import styles from './AboutPage.module.css';
 
 function AboutPage() {
@@ -7,6 +8,8 @@ function AboutPage() {
     return (
         <section className={styles['about-page']}>
             <Heading text={'О нас'} />
+
+            <FeedbackFrom />
         </section>
     )
 }
--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -1,13 +1,10 @@
 import EnterFrom from "../../components/enterForm/EnterFrom";
-import FeedbackFrom from "../../components/feedbackForm/FeedbackFrom";
 
 function LoginPage() {
   return (
     <section>
       <h2 className="visually-hidden">Страница входа пользователя</h2>
       <EnterFrom />
-
-      <FeedbackFrom />
     </section>
   )
 }
