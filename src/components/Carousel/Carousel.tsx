5b5a455e296eb0f438b4b106e4c123c5b220933d
Zhila <ministrov2018@gmail.com>
2024-12-03 14:18:46 +0300

    feature: start to develop a carousel component

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,6 +1,17 @@
+import useEmblaCarousel from "embla-carousel-react";
+import styles from './Carousel.module.css';
+
 function Carousel() {
+    const [emblaRef] = useEmblaCarousel();
+    // console.log(emblaRef);
     return (
-        <div>EmblaCarousel</div>
+        <div className={styles['embla']} ref={emblaRef}>
+            <div className={styles['embla__container']}>
+                <div className={styles['embla__slide']}>Slide 1</div>
+                <div className={styles['embla__slide']}>Slide 2</div>
+                <div className={styles['embla__slide']}>Slide 3</div>
+            </div>
+        </div>
     )
 }
 
